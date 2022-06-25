5ed8ef677ab15f32c6f34e18d283e4d829394b09
diego_barbosa <diego.barbosa.external@sgs.com>
2022-06-25 11:30:06 -0300

    Configuração de icones e splashScreen

---
+++ lib/util/const.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const bool const_debugMode = true;
+const String const_appName = "Teste Uol";
+const String const_apiKey = "e2b3d3f8fbea936f01252c2f5b0762bf";
+const String BASE_URL = "";
+
+final uolTheme = ThemeData(
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.light,
+  backgroundColor: const Color(0xFF212121),
+  accentColor: Colors.white,
+  accentIconTheme: IconThemeData(color: Colors.black),
+  dividerColor: Colors.black12,
+);
+final ThemeData const_theme = uolTheme;
